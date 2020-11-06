cbcbbc9733fd95bbd2cfa4fd0172d702d0cf691f
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-06 13:31:45 -0500

    Add filtering for user skins vs all skins

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -2,19 +2,31 @@ import "./Results.scss";
 import React, {useState, useEffect} from "react";
 import {getUserSkins, getAllSkins} from "../../apiCalls";
 
-export const Results = () => {
+export const Results = (props) => {
   const [neededSkins, setNeeededSkins] = useState({
     armor: [],
     weapons: [],
     dyes: [],
   })
   const getNeededSkins = async () => {
-    const res = await getUserSkins()
-    console.log(res)
+    const userSkins = await getUserSkins()
+    const allSkins = await getAllSkins()
+    let filteredSkins = allSkins.filter(skin => {
+      return !userSkins.includes(skin)
+    })
+    console.log(filteredSkins)
+    //const joined = res.join(',')
+    //filterSkinsByType(joined.slice(0, 100))
+  }
+  const filterSkinsByType = async (skins) => {
+    //const res = await getAllSkins(skins)
+    //console.log(res)
   }
   useEffect(() => {
+    console.log(props.match.params)
     getNeededSkins()
-  }, [])
+
+  })
 
   return (
     <div className="results">
