a235d09664bc52cb0352619f23da1ab456263636
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-06 01:41:57 -0500

    Remove old css rile, fix checkbox test

--- src/Components/Homepage/Homepage.test.js
+++ src/Components/Homepage/Homepage.test.js
@@ -1,5 +1,7 @@
 import {render, screen} from '@testing-library/react';
 import {Homepage} from './Homepage';
+import {MemoryRouter} from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 describe("BrowsePage", () => {
   it("Homepage should load with header, and checkboxes", async () => {
@@ -8,6 +10,15 @@ describe("BrowsePage", () => {
     expect(screen.getByText(/Armor/i)).toBeInTheDocument();
     expect(screen.getByText(/Weapons/i)).toBeInTheDocument();
     expect(screen.getByText(/Dyes/i)).toBeInTheDocument();
+  });
+
+  it("Checkbox should fire selectionsToUpdate", async () => {
+    const mockSelectionsToUpdate = jest.fn()
+    render(<Homepage updateSelections={mockSelectionsToUpdate} />);
+    userEvent.click(screen.getByTestId('armor-test'))
+    userEvent.click(screen.getByTestId('weapons-test'))
+    userEvent.click(screen.getByTestId('dyes-test'))
+    expect(mockSelectionsToUpdate).toHaveBeenCalledTimes(3);
     screen.debug()
   });
 
