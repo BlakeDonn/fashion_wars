4c9e897050bac4d794a66f65c6f88a1a68d55db8
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-06 01:13:41 -0500

    Add names to weapons and dyes

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -10,10 +10,10 @@ export const Homepage = (props) => {
           <div className="checkbox-container">
             <h3 className="check-header">Which skins would you like to find?</h3>
             <input type="checkbox" className="armor" name="armor" value="armor" onChange={(e) => props.updateSelections(e)}></input>
-            <label for="armor" className="armor-label">Armor</label>
-            <input type="checkbox" className="weapons"></input>
-            <label for="weapons" className="weapons-label">Weapons</label>
-            <input type="checkbox" className="dyes"></input>
+            <label for="armor" className="armor-label" >Armor</label>
+            <input type="checkbox" className="weapons" name="weapons" onChange={(e) => props.updateSelections(e)}></input>
+            <label for="weapons" className="weapons-label" >Weapons</label>
+            <input type="checkbox" className="dyes" name="dyes" onChange={(e) => props.updateSelections(e)}></input>
             <label for="dyes" className="dyes-label">Dyes</label>
             <button className='skin-submit'>Find skins!</button>
           </div>
