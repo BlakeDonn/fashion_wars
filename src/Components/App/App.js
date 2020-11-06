2a775eec31971e04ce09a94d05cd747eece86f9d
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-06 01:50:40 -0500

    Import route, add initial route

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,21 +1,26 @@
-import './App.css';
-import {Homepage} from '../Homepage/Homepage';
-import React, {useState} from 'react';
+import "./App.css";
+import {Homepage} from "../Homepage/Homepage";
+import React, {useState} from "react";
+import {Route} from "react-router-dom";
 
 export const App = () => {
-  const [selections, setSelections] = useState({armor: false, weapons: false, dyes: false});
+  const [selections, setSelections] = useState({
+    armor: false,
+    weapons: false,
+    dyes: false,
+  });
   const updateSelections = (e) => {
-    let selectionsToUpdate = selections
-    selectionsToUpdate[e.target.name] = !selectionsToUpdate[e.target.name]
-    setSelections(selectionsToUpdate)
-  }
+    let selectionsToUpdate = selections;
+    selectionsToUpdate[e.target.name] = !selectionsToUpdate[e.target.name];
+    setSelections(selectionsToUpdate);
+  };
   return (
     <div className="App">
       <header className="">
-        <Homepage updateSelections={updateSelections} />
+        <Route path="/">
+          <Homepage updateSelections={updateSelections} />
+        </Route>
       </header>
     </div>
   );
-}
-
-
+};
