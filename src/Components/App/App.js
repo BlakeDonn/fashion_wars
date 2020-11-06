b9912226df8ff62774c1c2c988ab9378f1bad70c
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-06 01:13:24 -0500

    Fixed issue with update app state directly

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,7 +6,7 @@ export const App = () => {
   const [selections, setSelections] = useState({armor: false, weapons: false, dyes: false});
   const updateSelections = (e) => {
     let selectionsToUpdate = selections
-    selections[e.target.name] = !selections[e.target.name]
+    selectionsToUpdate[e.target.name] = !selectionsToUpdate[e.target.name]
     setSelections(selectionsToUpdate)
   }
   return (
