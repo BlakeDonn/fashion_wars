4e9a3f92fec388ead97872d6073bd6ee7bf652f8
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-06 01:05:09 -0500

    Add state hook and selectionsToUpdate function

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import {Homepage} from '../Homepage/Homepage';
+import React, {useState} from 'react';
 
-export const  App = () => {
+export const App = () => {
+  const [selections, setSelections] = useState({armor: false, weapons: false, dyes: false});
+  const updateSelections = (e) => {
+    let selectionsToUpdate = selections
+    selections[e.target.name] = !selections[e.target.name]
+    setSelections(selectionsToUpdate)
+  }
   return (
     <div className="App">
       <header className="">
-        <Homepage />
+        <Homepage updateSelections={updateSelections} />
       </header>
     </div>
   );
