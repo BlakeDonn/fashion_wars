39cd3155c3b587e188c58a058d7c9c1986886861
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-06 02:27:52 -0500

    Add api calls

---
+++ src/apiCalls.js
@@ -0,0 +1,17 @@
+export const getUserSkins = async () => {
+  const response = await fetch('https://api.guildwars2.com/v2/account/skins?access_token=DC2293C1-9CF6-1C4D-A163-D8EF10B303CFF8EAC69C-ED5A-4F1B-B67E-410CCBF57CD4')
+  if (response.ok) {
+    return await response.json()
+  } else {
+    return response.error
+  }
+}
+
+export const geAllSkins = async () => {
+  const response = await fetch('https://wiki.guildwars2.com/wiki/API:1/skins')
+  if (response.ok) {
+    return await response.json()
+  } else {
+    return response.error
+  }
+}
