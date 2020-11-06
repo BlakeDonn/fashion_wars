e16d5518d348f895804af0302446527ab0fce50e
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-06 13:15:17 -0500

    Move useState out of app to homepage

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,29 +1,17 @@
 import "./App.css";
 import {Homepage} from "../Homepage/Homepage";
 import {Results} from "../Results/Results";
-import React, {useState} from "react";
 import {Route} from "react-router-dom";
 
 export const App = () => {
-  const [selections, setSelections] = useState({
-    armor: false,
-    weapons: false,
-    dyes: false,
-  });
-  const updateSelections = (e) => {
-    let selectionsToUpdate = selections;
-    selectionsToUpdate[e.target.name] = !selectionsToUpdate[e.target.name];
-    setSelections(selectionsToUpdate);
-  };
-  console.log(process.env.REACT_APP_API_KEY)
   return (
     <div className="App">
       <header className="">
         <Route exact path="/">
-          <Homepage updateSelections={updateSelections} />
+          <Homepage />
         </Route>
         <Route exact path="/results">
-          <Results selectedSkins={selections} />
+          <Results />
         </Route>
       </header>
     </div>
