b566203dfea98252c8aefdb75660d1d4fe9bb7a4
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-05 23:38:27 -0500

    Add initial checkboxes for filter

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -4,9 +4,23 @@ export const Homepage = () => {
   return (
     <div className="parent">
       <section className="container">
-      <header className="home-header">Header</header>
-      <main className="home-main">HomePage</main>
-      <footer className="home-footer">Footer Content</footer>
+        <header className="home-header">Header</header>
+        <main className="home-main">
+          <h2>Welcome To Fasion Wars !</h2>
+          <ul>
+            <li><input type="checkbox"
+          ></input>Armor
+            </li>
+
+            <li><input type="checkbox"
+          ></input>Armor
+            </li>
+            <li><input type="checkbox"
+          ></input>Armor
+            </li>
+          </ul>
+        </main>
+        <footer className="home-footer">Footer Content</footer>
       </section>
     </div>
   );
