f186759ba6d383b164a611b9a96f7ffc6d13b473
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-06 12:28:09 -0500

    Adjust wording on environmental variable

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,5 @@
 export const getUserSkins = async () => {
-  const response = await fetch(`https://api.guildwars2.com/v2/account/skins?access_token=${process.env.API_KEY}`)
+  const response = await fetch(`https://api.guildwars2.com/v2/account/skins?access_token=${process.env.REACT_APP_API_KEY}`)
   if (response.ok) {
     return await response.json()
   } else {
@@ -7,11 +7,13 @@ export const getUserSkins = async () => {
   }
 }
 
-export const geAllSkins = async () => {
+export const getAllSkins = async () => {
   const response = await fetch('https://api.guildwars2.com/v1/skins.json')
   if (response.ok) {
+    console.log(response)
     return await response.json()
   } else {
     return response.error
   }
 }
+
