96cd097a57a4f5b16936832c60a94919b7bd85e4
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-05 22:21:29 -0500

    Remove whitesace

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import {Homepage} from '../Homepage/Homepage';
 
 export const  App = () => {
   return (
     <div className="App">
       <header className="App-header">
-          Learn React
+        <Homepage />
       </header>
     </div>
   );
