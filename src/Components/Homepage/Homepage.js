2fafc33b260b7a586d24b8a4506cf27338e01d41
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-05 21:28:54 -0500

    Add initial structure for Homepage

---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import './Homepage.css';
+
+export const  Homepage = () => {
+  return (
+    <div className="Homepage">
+      <header className="Homepage-header">
+          Learn React
+      </header>
+    </div>
+  );
+}
+
+
