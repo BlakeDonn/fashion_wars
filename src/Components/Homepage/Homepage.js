d619dc7a6c6a92be697a8eb5dcef299469fe8546
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-06 13:16:43 -0500

    Add useState and pass user selected value on link

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,7 +1,21 @@
 import "./Homepage.scss";
-import {Link} from 'react-router-dom'
+import {Link} from "react-router-dom";
+import React, {useState} from "react";
 
 export const Homepage = (props) => {
+  const [selections, setSelections] = useState({
+    armor: false,
+    weapons: false,
+    dyes: false,
+  });
+  const updateSelections = (e) => {
+    let selectionsToUpdate = selections;
+    selectionsToUpdate[e.target.name] = !selectionsToUpdate[e.target.name];
+    setSelections(selectionsToUpdate);
+  };
+  const determineStateToPost = () => {
+    return `/results/:${Object.values(selections)}`;
+  };
   const skinTypes = () => {
     let skins = ["armor", "weapons", "dyes"];
     return skins.map((skin, i) => {
@@ -11,11 +25,11 @@ export const Homepage = (props) => {
             key={i}
             type="checkbox"
             className={skin}
-            data-testid={`${skin}-test`}
+            data-testid={`${skin} -test`}
             name={skin}
-            onClick={(e) => props.updateSelections(e)}
+            onClick={updateSelections}
           ></input>
-          <label htmlFor="armor" className={`${skin}-label`}>
+          <label htmlFor="armor" className={`${skin} -label`}>
             {skin}
           </label>
         </div>
@@ -33,7 +47,9 @@ export const Homepage = (props) => {
               Which skins would you like to find?
             </h3>
             {skinTypes()}
-            <Link to='/results'><button className="skin-submit">Find skins!</button></Link>
+            <Link to={determineStateToPost}>
+              <button className="skin-submit">Find skins!</button>
+            </Link>
           </div>
         </main>
         <footer className="home-footer">Footer Content</footer>
