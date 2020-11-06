73e6be686a6d2c661d9dca1b8c02484e97b27284
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-06 01:54:36 -0500

    Add memory router to component render

--- src/Components/Homepage/Homepage.test.js
+++ src/Components/Homepage/Homepage.test.js
@@ -1,11 +1,15 @@
-import {render, screen} from '@testing-library/react';
-import {Homepage} from './Homepage';
-import {MemoryRouter} from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
+import {render, screen} from "@testing-library/react";
+import {Homepage} from "./Homepage";
+import {MemoryRouter} from "react-router-dom";
+import userEvent from "@testing-library/user-event";
 
 describe("BrowsePage", () => {
   it("Homepage should load with header, and checkboxes", async () => {
-    render(<Homepage />);
+    render(
+      <MemoryRouter>
+        <Homepage />
+      </MemoryRouter>
+    );
     expect(screen.getByText(/Fashion/i)).toBeInTheDocument();
     expect(screen.getByText(/Armor/i)).toBeInTheDocument();
     expect(screen.getByText(/Weapons/i)).toBeInTheDocument();
@@ -13,12 +17,15 @@ describe("BrowsePage", () => {
   });
 
   it("Checkbox should fire selectionsToUpdate", async () => {
-    const mockSelectionsToUpdate = jest.fn()
-    render(<Homepage updateSelections={mockSelectionsToUpdate} />);
-    userEvent.click(screen.getByTestId('armor-test'))
-    userEvent.click(screen.getByTestId('weapons-test'))
-    userEvent.click(screen.getByTestId('dyes-test'))
+    const mockSelectionsToUpdate = jest.fn();
+    render(
+      <MemoryRouter>
+        <Homepage updateSelections={mockSelectionsToUpdate} />
+      </MemoryRouter>
+    );
+    userEvent.click(screen.getByTestId("armor-test"));
+    userEvent.click(screen.getByTestId("weapons-test"));
+    userEvent.click(screen.getByTestId("dyes-test"));
     expect(mockSelectionsToUpdate).toHaveBeenCalledTimes(3);
   });
-
 });
