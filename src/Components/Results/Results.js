1de9b300180f06f40e393512e7fca7d893a2a003
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-07 15:51:05 -0500

    Add initial structure for loop through skins

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -3,39 +3,44 @@ import React, {useState, useEffect} from "react";
 import {getUserSkins, getAllSkins, getFilteredSkins} from "../../apiCalls";
 
 export const Results = (props) => {
-  const [neededSkins, setNeeededSkins] = useState({
-    armor: [],
-    weapons: [],
-    dyes: [],
-  })
+  const [neededSkins, setNeeededSkins] = useState('{}');
   const getNeededSkins = async () => {
-    const userSkins = await getUserSkins()
-    const allSkins = await getAllSkins()
-    return allSkins.filter(skin => {
-      return !userSkins.includes(skin)
-    })
-  }
+    const userSkins = await getUserSkins();
+    const allSkins = await getAllSkins();
+    return allSkins.filter((skin) => {
+      return !userSkins.includes(skin);
+    });
+  };
   const filterSkinsByType = async (skins) => {
-    const allNeededSkins = await getNeededSkins()
-    const joinedSkins = allNeededSkins.join(',').slice(0, 500)
-    const skinsForUser = await getFilteredSkins(joinedSkins)
-    console.log(skinsForUser)
-  }
-  useEffect(() => {
-    console.log(props.match.params)
-    filterSkinsByType()
+    const allNeededSkins = await getNeededSkins();
+    let counter = Math.floor(allNeededSkins.length / 200);
+    let i = 1;
+    let start = 0
+    stateHolder = {
+      armor: [],
+      weapons: [],
+      dyes: [],
+    }
+    while (i < counter) {
+      const joinedSkins = allNeededSkins.join(",").slice(start, 200);
+      const skinsForUser = await getFilteredSkins(joinedSkins);
 
-  })
+    }
+  };
+  useEffect(() => {
+    console.log(props.match.params);
+    filterSkinsByType();
+  });
 
   return (
     <div className="results">
-      <header className='results-header'>
+      <header className="results-header">
         <h1>Skins you need to unlock!</h1>
       </header>
       <div className="left-sidebar">right-side-for-armor</div>
-      <main className='results-main'>right-side-for-weapons</main>
+      <main className="results-main">right-side-for-weapons</main>
       <div className="right-sidebar">right-side-for-dyes</div>
-      <footer className='results-footer'></footer>
+      <footer className="results-footer"></footer>
     </div>
   );
 };
