e4a7316d224711a9ff939b1fed4adbdec0d2fd33
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-07 20:52:39 -0500

    Add next two integration test

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,6 +1,9 @@
-import {render, screen} from "@testing-library/react";
+import {render, screen, waitFor} from "@testing-library/react";
 import {MemoryRouter} from "react-router-dom";
 import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import {getUserSkins, getAllSkins, getFilteredSkins} from "../../apiCalls.js";
+jest.mock("../../apiCalls.js");
 import {App} from "./App";
 
 describe("BrowsePage", () => {
@@ -12,13 +15,65 @@ describe("BrowsePage", () => {
     );
     expect(screen.getByText(/Fashion/i)).toBeInTheDocument();
   });
-  it("User should be brought to results page when clicking skin submit button", async () => {
+
+  it("User should be able to get to the results page and see their skins", async () => {
+    getUserSkins.mockResolvedValue(new Array(200).fill().map((_, i) => (i)))
+    getAllSkins.mockResolvedValue(new Array(400).fill().map((_, i) => (i)))
+    getFilteredSkins.mockResolvedValue([
+      {
+        "name": "Bifrost",
+        "type": "Weapon",
+      },
+      {
+        "name": "Invisible Boots",
+        "type": "Armor",
+      },
+      {
+        "name": "Ad-Infinium",
+        "type": "Back",
+      }
+    ])
     render(
       <MemoryRouter>
         <App />
       </MemoryRouter>
     );
+    userEvent.click(screen.getByTestId("Armor-test"));
+    userEvent.click(screen.getByTestId("Weapons-test"));
+    userEvent.click(screen.getByTestId("Back-test"));
     userEvent.click(screen.getByRole('button', {name: 'Find skins!'}));
-    expect(screen.getByText(/Skins you need to unlock!/i)).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByText(/Skins you need to unlock!/i)).toBeInTheDocument())
+    expect(screen.getByText(/Bifrost/i)).toBeInTheDocument()
+    expect(screen.getByText(/Invisible Boots/i)).toBeInTheDocument()
+    expect(screen.getByText(/Ad-Infinium/i)).toBeInTheDocument()
+  });
+
+  it("User should only see the categories they select", async () => {
+    getUserSkins.mockResolvedValue(new Array(200).fill().map((_, i) => (i)))
+    getAllSkins.mockResolvedValue(new Array(400).fill().map((_, i) => (i)))
+    getFilteredSkins.mockResolvedValue([
+      {
+        "name": "Bifrost",
+        "type": "Weapon",
+      },
+      {
+        "name": "Invisible Boots",
+        "type": "Armor",
+      },
+      {
+        "name": "Ad-Infinium",
+        "type": "Back",
+      }
+    ])
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    userEvent.click(screen.getByTestId("Armor-test"));
+    userEvent.click(screen.getByTestId("Weapons-test"));
+    userEvent.click(screen.getByRole('button', {name: 'Find skins!'}));
+    await waitFor(() => expect(screen.getByText(/Skins you need to unlock!/i)).toBeInTheDocument())
+    expect(screen.getByText(/Bifrost/i)).toBeInTheDocument()
   });
 });
