34696f5684f69df79b362a3a6036bcea28d92cdf
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-09 03:31:48 -0500

    Add two unit tests

--- src/Components/PreviewSkin/PrevieSkin.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import {PreviewSkin} from './PreviewSkin';
-
-test('renders with Skin', () => {
-  render(<PreviewSkin />);
-  const linkElement = screen.getByText(/Skin/i);
-  expect(linkElement).toBeInTheDocument();
-});
