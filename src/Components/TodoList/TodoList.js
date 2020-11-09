7ad819ed607ada728e49269b661cd573d042f5c0
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-09 18:49:46 -0500

    Add initial structure for todo list

---
+++ src/Components/TodoList/TodoList.js
@@ -0,0 +1,9 @@
+import "./TodoList.scss";
+import {Link} from "react-router-dom";
+export const TodoList = (props) => {
+  console.log(props)
+  return (
+    <div className="todo-list">
+    </div>
+  );
+};
