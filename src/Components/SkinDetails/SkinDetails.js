2d6316d214e26568ef4575338ce58f4c01807368
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-09 18:42:16 -0500

    Reformat for linter and remove logs

--- src/Components/SkinDetails/SkinDetails.js
+++ src/Components/SkinDetails/SkinDetails.js
@@ -7,25 +7,25 @@ export class SkinDetails extends Component {
     super(props);
     this.state = {
       skinSpecs: props.location.skinDetails,
-      url: ""
+      url: "",
     };
   }
   componentDidMount = async () => {
-    let imageUrl = await getImage(this.state.skinSpecs.name)
-    console.log(imageUrl)
-    this.setState({url: imageUrl})
-  }
+    let imageUrl = await getImage(this.state.skinSpecs.name);
+    console.log(imageUrl);
+    this.setState({url: imageUrl});
+  };
   determineImageDetails = () => {
     if (!this.state.url.length) {
-      return
+      return;
     }
-    if (this.state.url.includes('wiki')) {
-      return `${this.state.skinSpecs.name} set preview`
+    if (this.state.url.includes("wiki")) {
+      return `${this.state.skinSpecs.name} set preview`;
     }
-    if (this.state.url.includes('imgur')) {
-      return 'No image found, please check wiki link for more info'
+    if (this.state.url.includes("imgur")) {
+      return "No image found, please check wiki link for more info";
     }
-  }
+  };
 
   determineSkinType = () => {
     if (this.state.skinSpecs.type === "Armor") {
@@ -36,7 +36,7 @@ export class SkinDetails extends Component {
           <p>Armor Type: {this.state.skinSpecs.details.type}</p>
           <p>Rarity: {this.state.skinSpecs.rarity}</p>
         </div>
-      )
+      );
     }
     if (this.state.skinSpecs.type === "Weapon") {
       return (
@@ -46,15 +46,15 @@ export class SkinDetails extends Component {
           <p>Weapon Type: {this.state.skinSpecs.details.type}</p>
           <p>Rarity: {this.state.skinSpecs.rarity}</p>
         </div>
-      )
+      );
     }
     return (
       <div>
         <p>Type: {this.state.skinSpecs.type}</p>
         <p>Rarity: {this.state.skinSpecs.rarity}</p>
       </div>
-    )
-  }
+    );
+  };
   render() {
     return (
       <div className="skin-details">
@@ -62,14 +62,40 @@ export class SkinDetails extends Component {
           <h3>{this.state.skinSpecs.name}</h3>
           {this.determineSkinType()}
           <div className="bottom-buttons">
-            <a href={`https://wiki.guildwars2.com/wiki/${this.state.skinSpecs.name}`}><img src={this.state.skinSpecs.icon} alt={this.state.skinSpecs.name} className={"icon"} /></a>
+            <a
+              data-testid={"wiki-icon"}
+              href={`https://wiki.guildwars2.com/wiki/${this.state.skinSpecs.name}`}
+            >
+              <img
+                src={this.state.skinSpecs.icon}
+                alt={this.state.skinSpecs.name}
+                className={"icon"}
+              />
+            </a>
             <p className={"icon-text"}>More info</p>
-            <p onClick={() => this.props.location.updateList(this.state.skinSpecs)}><img src={"https://i.imgur.com/YhTbWKz.png"} className={"todo"} alt={"todo-icon"} /></p>
+            <p
+              onClick={() =>
+                this.props.location.updateList(this.state.skinSpecs)
+              }
+            >
+              <img
+                src={"https://i.imgur.com/YhTbWKz.png"}
+                className={"todo"}
+                alt={"todo-icon"}
+              />
+            </p>
             <p className={"todo-text"}>Add to list</p>
           </div>
         </div>
-        {this.state.url && <img className={"sidebar"} src={this.state.url} data-testid={"preview-test"} alt={this.determineImageDetails()} />}
+        {this.state.url && (
+          <img
+            className={"sidebar"}
+            src={this.state.url}
+            data-testid={"preview-test"}
+            alt={this.determineImageDetails()}
+          />
+        )}
       </div>
     );
   }
-};
+}
