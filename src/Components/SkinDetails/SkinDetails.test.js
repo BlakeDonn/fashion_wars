f4c7a839994e51b840eb49fcc3bb61c57a355c6d
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-09 14:18:50 -0500

    Add first two unit tests

---
+++ src/Components/SkinDetails/SkinDetails.test.js
@@ -0,0 +1,33 @@
+import {SkinDetails} from './SkinDetails';
+import {render, screen, waitFor} from "@testing-library/react";
+import {MemoryRouter} from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+jest.mock("../../apiCalls.js");
+
+describe('renders with Skin', () => {
+  it("Should load a preview of the skins icon", () => {
+    const testTry =
+    {
+      "name": "Exalted Coat",
+      "icon": "Test Icon",
+      "type": "Weapon",
+      "id": "250"
+    }
+    render(<MemoryRouter><SkinDetails location={{skinDetails: testTry}} /></MemoryRouter>)
+    expect(screen.getByText(/Exalted Coat/i)).toBeInTheDocument()
+  });
+
+  it("Should load a different type of skin", () => {
+    const testTry =
+    {
+      "name": "Invisible Boots",
+      "icon": "Test Icon",
+      "type": "Armor",
+      "id": "100"
+    }
+    render(<MemoryRouter><SkinDetails location={{skinDetails: testTry}} /></MemoryRouter>)
+    expect(screen.getByText(/Invisible Boots/i)).toBeInTheDocument()
+  });
+
+});
