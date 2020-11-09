830df50160faac2913986a0c26e75b3d6803aaf6
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-08 23:09:21 -0500

    Add route for specific skin

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import {Homepage} from "../Homepage/Homepage";
 import {Results} from "../Results/Results";
+import {SkinDetails} from "../SkinDetails/SkinDetails";
 import {Route} from "react-router-dom";
 
 export const App = () => {
@@ -9,6 +10,7 @@ export const App = () => {
       <header className="">
         <Route exact path="/"><Homepage /></Route>
         <Route exact path="/results/:results" component={Results} />
+        <Route exact path="/skin/:id" component={SkinDetails} />
       </header>
     </div >
   );
