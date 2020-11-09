4f76140a3754a5918635015d984dafe1e8eb555b
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-09 02:38:35 -0500

    Remove name from previewSkin

--- src/Components/PreviewSkin/PreviewSkin.js
+++ src/Components/PreviewSkin/PreviewSkin.js
@@ -1,17 +1,13 @@
 import "./PreviewSkin.scss";
 import {Link} from "react-router-dom";
-
 export const PreviewSkin = (props) => {
   return (
     <div className="preview-skin">
-      <header className="preview-skin-header">
-        <h3>{props.details.name}</h3>
-        <Link
-          to={{pathname: `${props.details.id}`, skinDetails: props.details}}
-        >
-          <img src={props.details.icon} alt={props.details.name} />
-        </Link>
-      </header>
-    </div>
+      <Link
+        to={{pathname: `/skin/${props.details.id}`, skinDetails: props.details}}
+      >
+        <img src={props.details.icon} alt={props.details.name} />
+      </Link>
+    </div >
   );
 };
