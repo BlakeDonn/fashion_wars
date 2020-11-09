8463bf533d263ad01d8261d62e9676c99618ae4e
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-08 22:39:52 -0500

    Add link functionality to skins

--- src/Components/Skin/Skin.js
+++ src/Components/Skin/Skin.js
@@ -1,11 +1,12 @@
 import './Skin.css';
+import {Link} from 'react-router-dom'
 
 export const Skin = (props) => {
-  console.log(props)
   return (
     <div className="Skin">
       <header className="skin-header">
-        <img src={props.details.icon} alt={props.details.name} />
+        <h3>{props.details.name}</h3>
+        <Link to={`${props.details.id}`}><img src={props.details.icon} alt={props.details.name} /></Link>
       </header>
     </div>
   );
