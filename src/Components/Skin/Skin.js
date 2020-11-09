0d5bc3f9dcccd5bf665c9f18ff47c5f20a5e6cbf
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-08 22:25:12 -0500

    Add image render for skins

--- src/Components/Skin/Skin.js
+++ src/Components/Skin/Skin.js
@@ -1,10 +1,11 @@
 import './Skin.css';
 
-export const  Skin = () => {
+export const Skin = (props) => {
+  console.log(props)
   return (
     <div className="Skin">
       <header className="skin-header">
-          Skin 
+        <img src={props.details.icon} alt={props.details.name} />
       </header>
     </div>
   );
