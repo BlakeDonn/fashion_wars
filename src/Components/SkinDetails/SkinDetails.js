a64ebc7880a98c17c2d0b90e9549e2d6756b1935
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-09 03:41:51 -0500

    Remove logs

--- src/Components/SkinDetails/SkinDetails.js
+++ src/Components/SkinDetails/SkinDetails.js
@@ -24,8 +24,8 @@ export const SkinDetails = (props) => {
     <div className="skin-details">
       <div className="content">
         <h1>{skinSpecs.name}</h1>
-        <p>Type: {skinSpecs.details.type}</p>
-        <p>Weight Class: {skinSpecs.details.weight_class}</p>
+        <p>Type: {skinSpecs.type}</p>
+        <p>Weight Class: {skinSpecs.detials && skinSpecs.details.weight_class}</p>
         <p>Rarity: {skinSpecs.rarity}</p>
         <a href={`https://wiki.guildwars2.com/wiki/${skinSpecs.name}`}><img src={skinSpecs.icon} alt={skinSpecs.name} /></a>
         <a href={`https://wiki.guildwars2.com/wiki/${skinSpecs.name}`}>More info</a>
