35a031764beb68782fa153a2e2bc925204fb00e9
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-09 19:14:16 -0500

    Add mapping ot display todo skins

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,9 +1,24 @@
 import "./TodoList.scss";
 import {Link} from "react-router-dom";
 export const TodoList = (props) => {
-  console.log(props)
-  return (
-    <div className="todo-list">
-    </div>
-  );
+  const distributeSkins = () => {
+    return props.location.todoSkins.map((skin) => {
+      return (
+        <div className="skin-container">
+          <div>
+            <h3>{skin.name}</h3>
+            <p>{skin.type}</p>
+            <img
+              src={skin.icon}
+              alt={skin.name}
+              className={"icon"}
+            />
+          </div>
+        </div>
+      );
+    });
+  };
+  return <div className="todo-list">
+    {distributeSkins()}
+  </div>;
 };
