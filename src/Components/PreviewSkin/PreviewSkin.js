22a1530716c0a95ae4c2a8f467a9a06719ae5b69
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-09 21:51:15 -0500

    Finalize linter

--- src/Components/PreviewSkin/PreviewSkin.js
+++ src/Components/PreviewSkin/PreviewSkin.js
@@ -1,5 +1,5 @@
 import "./PreviewSkin.scss";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 export const PreviewSkin = (props) => {
   return (
     <div className="preview-skin">
@@ -8,7 +8,7 @@ export const PreviewSkin = (props) => {
           pathname: `/results/skins/${props.details.id}`,
           skinDetails: props.details,
           updateList: props.updateList,
-          userList: props.userList
+          userList: props.userList,
         }}
       >
         <img
